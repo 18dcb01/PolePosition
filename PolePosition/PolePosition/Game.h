222a1a32dbad19b34368a5087e526192d43af98d
Valerie <35704201+vbesser26@users.noreply.github.com>
2018-01-22 14:04:14 -0600

    .h mostly finished

--- PolePosition/PolePosition/Game.h
+++ PolePosition/PolePosition/Game.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "Player.h"
+#include <vector>
+
 class Game
 {
 public:
+	Game();
+	~Game();
+	void play();
+
 private:
+	Player p;
+	Racer r[7];
+	std::vector<Direction> map;
+	std::vector<Object> signs;
+	int score;
+	time_t startTime;
+	void race();
+	void tick();
+	void render();
+	void drawMap();
+	void drawBackground();
+
 };
