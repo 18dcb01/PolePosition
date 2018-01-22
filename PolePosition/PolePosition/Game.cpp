2da599961ad12ac9f9b40d016c5b3aa992c3525b
Valerie <35704201+vbesser26@users.noreply.github.com>
2018-01-22 14:17:42 -0600

    Game.cpp

---
+++ PolePosition/PolePosition/Game.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+
+Game::~Game()
+{
+
+}
+
+
+void Game::play()
+{
+	//Calls race (twice bc two races)
+}
+
+
+void Game::race()
+{
+	//A loop - continually calls tick
+}
+
+
+void Game::tick()
+{
+	//Calls render, updates player and racers
+}
+
+
+void Game::render()
+{
+	//First, drawBackground
+	//Then, drawMap
+	//Then signs, racers, and the player
+}
+
+
+void Game::drawMap()
+{
+
+}
+
+
+void Game::drawBackground()
+{
+
+}
