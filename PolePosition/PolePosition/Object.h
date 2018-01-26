89a3e6ce7342671d925986ae5f049cbca0b14e3c
Michael Adams <31117788+Madams18@users.noreply.github.com>
2018-01-26 12:52:11 -0600

    something

--- PolePosition/PolePosition/Object.h
+++ PolePosition/PolePosition/Object.h
@@ -7,7 +7,15 @@ public:
 	Object();
 	~Object();
 	virtual void render(int);
+<<<<<<< HEAD
 private:
+=======
+	void setPos(int, int);
+	int getPosx();
+	int getPosy();
+
+protected:
+>>>>>>> 803da4b1382250e8aef6ca4c31b47095e416d91a
 	sf::Sprite sprite;
 	int position[2];
 };
--- PolePosition/PolePosition/Racer.cpp
+++ PolePosition/PolePosition/Racer.cpp
@@ -1,2 +1,19 @@
 #include "stdafx.h"
+<<<<<<< HEAD
 #include "Racer.h"
+=======
+#include "Racer.h"
+
+Racer::Racer() : Car()
+{
+}
+
+Racer::~Racer()
+{
+
+}
+
+void Racer::tick()
+{
+}
+>>>>>>> 803da4b1382250e8aef6ca4c31b47095e416d91a
