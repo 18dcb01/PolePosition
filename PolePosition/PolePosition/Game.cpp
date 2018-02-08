cff1478fc4efad1722a0b264ec8b515dbdd6b920
Michael Adams <31117788+Madams18@users.noreply.github.com>
2018-02-08 12:48:28 -0600

    ignore

--- PolePosition/PolePosition/Game.cpp
+++ PolePosition/PolePosition/Game.cpp
@@ -7,11 +7,10 @@ Game::Game(sf::RenderWindow *tempWindow)
 	window = tempWindow;
 
 	//creating background texture
-	if (!background.loadFromFile("PolePositionMtFuji.png")
-		cout << "Error loading background texture\n";
+	if (!background.loadFromFile("PolePositionMtFuji.png"))
+		std::cout << "Error loading background texture\n";
 	//applying texture to background sprite
 	backgroundSprite.setTexture(background);
-	backgroundSprite.setScale(.5, .5);
 
 	//Map is initialized to all straight for now.
 	for (int i = 0; i < 50; i++)
@@ -78,10 +77,10 @@ void Game::drawBackground()
 
 	//create background sprite
 	backgroundSprite.setTexture(background);
-	backgroundSprite.setScale(.5, .5);
+	backgroundSprite.setScale(1, 1.75);
 
 	//rotate sprite to correct angle
-	backgroundSprite.move(.1, 0);
+	backgroundSprite.move(-.1, 0);
 	if (backgroundSprite.getPosition().x < -windowSize.x)
 		backgroundSprite.setPosition(sf::Vector2f(0, 0));
 	if (backgroundSprite.getPosition().x > 0)
