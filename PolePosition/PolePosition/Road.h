d99654f7f54aa9376922422fb7f3b76f77be6411
Michael Adams <31117788+Madams18@users.noreply.github.com>
2018-04-24 13:36:19 -0500

    Everything refuses to work

--- PolePosition/PolePosition/Road.h
+++ PolePosition/PolePosition/Road.h
@@ -8,18 +8,6 @@
 
 class Road
 {
-/*
-TODO:
-
-Should be the class that defines the quadratic that will represent the
-curvature of the road.
-
- Uses road shapes to map sf objects to the aforementioned lines.
-
- moved loadTrack to road, plug it in
-*/
-
-
 public:
 	Road();
 	Road(sf::RenderWindow*);
